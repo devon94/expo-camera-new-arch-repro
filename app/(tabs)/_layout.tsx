8585e8acfe0fe79ca63d8ee83686a0d9c893b289
Devon Deonarine <hello@devondeonarine.ca>
2024-09-19 14:45:11 -0400

    Minimal Repro

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
+          title: 'Camers',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
